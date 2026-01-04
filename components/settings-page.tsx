d2e9e01b56b23baff60d3014bcd84817c36fdc0b
nattaratsampao <nattaratsampao@gmail.com>
2026-01-04 20:55:10 +0700

    Web

--- components/settings-page.tsx
+++ components/settings-page.tsx
@@ -21,8 +21,8 @@ export function SettingsPage() {
   const [lineNotifyEnabled, setLineNotifyEnabled] = useState(false)
   const [lineNotifyToken, setLineNotifyToken] = useState("")
   const [lineBotEnabled, setLineBotEnabled] = useState(false)
-  const [lineChannelAccessToken, setLineChannelAccessToken] = useState("")
-  const [lineChannelSecret, setLineChannelSecret] = useState("")
+  const [lineChannelAccessToken, setLineChannelAccessToken] = useState("j3crGSU4CYJa97muDOmjOBJjVhKC+SLsco1qi2QoErrKQPq20v3W6wmbv6JPNv6B6pZa1IKMoY4ym2FfOcgUWw04m1hm7SPP/03CsapI97bVRxyGxKl41zT3rVh4M+fxvDFHnIlWJjb1ovj+QLRP1gdB04t89/1O/w1cDnyilFU=")
+  const [lineChannelSecret, setLineChannelSecret] = useState("e494662992f69b8a117272368a8eec70")
   const [isSendingTest, setIsSendingTest] = useState(false)
   const { toast } = useToast()
 
